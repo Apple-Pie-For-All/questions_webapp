0498a39b9e090f91c34fe10ad334049f6a80fd04
Apple-Pie-For-All <mcdonnell32@gmail.com>
2024-09-11 22:56:03 -0700

    Modified db_alchemy.py to accept db URI for test injection

--- flaskr/db_alchemy.py
+++ flaskr/db_alchemy.py
@@ -2,14 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 import click
 
-engine = create_engine('sqlite:///instance/flaskr.sqlite')
 db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
+                                         autoflush=False))
 Base = declarative_base()
 Base.query = db_session.query_property()
 
-def init_db():
+def init_db(engine):
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
@@ -18,9 +16,13 @@ def init_db():
 
 @click.command('init-db')
 def init_db_command():
-    init_db()
+    init_db(db_session.bind)
     click.echo('Initialized the database with SQLalchemy.')
 
 def init_app(app):
+    # Setup
+    engine = create_engine(app.config['SQLALCHEMY_URI'])
+    db_session.configure(bind=engine)
+
     # app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
