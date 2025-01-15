0cd692d39efaaca28f9e90f1b1a398b3741ea6eb
Apple-Pie-For-All <mcdonnell32@gmail.com>
2025-01-14 22:40:27 -0800

    Updated user credentials for test auto login

--- tests/conftest.py
+++ tests/conftest.py
@@ -93,7 +93,7 @@ class AuthActions(object):
     def __init__(self, client):
         self._client = client
 
-    def login(self, username='test', password='test'):
+    def login(self, username='tester', password='test_password'):
         return self._client.post(
             '/auth/login',
             data={'username': username, 'password': password}
