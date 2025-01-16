4a11eb5f1d09e80f71c5d87f3b008df8e9c035d1
Apple-Pie-For-All <mcdonnell32@gmail.com>
2025-01-15 18:32:58 -0800

    Added tests for login

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -14,3 +14,16 @@ def test_register(client, app):
 
     with app.app_context():
         assert select(User).where(User.name=='a').one() is not None
+
+# Parametrized arguements for next function args
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('', '', b'Username is required.'),
+    ('a', '', b'Password is required.'),
+    ('tester', 'test_password', b'already registered'),
+))
+def test_register_validate_input(client, username, password, message):
+    response = client.post(
+        '/auth/register',
+        data={'username': username, 'password': password}
+    )
+    assert message in response.data
