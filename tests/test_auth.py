05cbe4cece356f5aab54cfd54d5908c34c242e2b
Apple-Pie-For-All <mcdonnell32@gmail.com>
2025-01-18 19:57:28 -0800

    Added comment to test_register in test_auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -6,6 +6,9 @@ from sqlalchemy import event, select
 
 
 def test_register(client, app):
+    """
+    Test that the register page is reachable and allows new users to regist
+    """
     assert client.get('/auth/register').status_code == 200
     response = client.post(
         '/auth/register', data={'username': 'a', 'password': 'a'}
