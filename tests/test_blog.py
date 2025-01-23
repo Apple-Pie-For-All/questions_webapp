1d76138b936435136723f91bd5e15f63c033c4f1
Apple-Pie-For-All <mcdonnell32@gmail.com>
2025-01-22 17:37:23 -0800

    Added test_delete to test_blog.py

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -106,3 +106,25 @@ def test_create_update_validate(client, auth, path):
     auth.login()
     response = client.post(path, data={'title': '', 'body': ''})
     assert b'Title is required.' in response.data
+
+def test_delete(client, auth, app):
+    """
+    Tests delete functionality
+    """
+    auth.login()
+    stmt = select(Post).where(Post.author_id == g.user['user_id'])
+    post_to_delete = session.scalars(stmt).first().id
+    url_for_delete = '/' + post_to_delete + '/delete'
+    response = client.post(url_for_delete)
+
+    # Should redirect to index when successful
+    assert response.headers["Location"] == "/"
+
+    with app.app_context():
+        stmt = select(Post).where(Post.id == post_to_delete)
+        post = session.scalars(stmt).first()
+        # db = get_db()
+        # post = db.execute('SELECT * FROM post WHERE id = 1').fetchone()
+
+        # Post should no longer exist
+        assert post is None
