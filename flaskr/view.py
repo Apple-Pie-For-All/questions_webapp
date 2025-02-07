37440c10e7aeaca5d062fa59b87d7fdf2342a74a
Apple-Pie-For-All <mcdonnell32@gmail.com>
2025-02-06 23:05:27 -0800

    Added the ability to create comments on the backend in view.py

--- flaskr/view.py
+++ flaskr/view.py
@@ -4,7 +4,7 @@ from flask import (
 from werkzeug.exceptions import abort
 from flaskr.auth import login_required
 from .db_alchemy import db_session
-from .data_model import User, Post
+from .data_model import User, Post, Comment
 from sqlalchemy import select
 from sqlalchemy.orm import joinedload
 
@@ -19,3 +19,19 @@ def view(id):
     post_for_page = db_session.scalars(stmt).first()
 
     return render_template('view/view.html', post=post_for_page)
+
+@bp.route('/<int:post_id>/comment', methods=("POST",))
+@login_required
+def add_comment(post_id):
+    """
+    Add a comment to a post
+    """
+    comment_body = request.form['text']
+    if comment_body == '':
+        flash("Comment text is required")
+    else:
+        new_comment = Comment(parent_post_id=post_id, author_id=g.user.id, text=comment_body)
+        db_session.add(new_comment)
+        db_session.commit()
+
+        return redirect(url_for(view.view, id=post_id))
